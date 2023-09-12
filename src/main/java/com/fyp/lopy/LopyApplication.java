7a6eecc7f56937cbc258af50f39c8765dded1833
bowen <xubowen495@gmail.com>
2023-09-12 23:46:34 +0800

    Project initial set up

---
+++ src/main/java/com/fyp/lopy/LopyApplication.java
@@ -0,0 +1,13 @@
+package com.fyp.lopy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LopyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LopyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fyp/lopy/LopyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fyp.lopy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LopyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

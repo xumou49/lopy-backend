1fc4c4f46407020255b66fcdec1710078e9fd5e6
Dex <>
2023-10-31 14:55:10 +0800

    java: test build flow

--- java-jerry/src/main/java/com/lopy/controller/DemoController.java
+++ java-jerry/src/main/java/com/lopy/controller/DemoController.java
@@ -3,10 +3,9 @@ package com.lopy.controller;
 import com.lopy.common.auth.Authorize;
 import com.lopy.common.constant.CommonConstant;
 import com.lopy.common.vo.RespVO;
-import com.lopy.service.biz.intf.AppleService;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(CommonConstant.API.V1_PATH + "/demo")
 @RestController
 public class DemoController {
-
-    @Autowired
-    private AppleService appleServiceImpl;
 
     @PostMapping("/list")
     @Authorize(access = {CommonConstant.Account.RESTAURATEUR, CommonConstant.Account.CUSTOMER})
@@ -50,6 +46,11 @@ public class DemoController {
     @DeleteMapping("/delete")
     @Authorize(access = CommonConstant.Account.RESTAURATEUR)
     public RespVO<Void> delete() {
+        return RespVO.ok();
+    }
+
+    @GetMapping("/test-build")
+    public RespVO<Void> testBuild() {
         return RespVO.ok();
     }
 }
